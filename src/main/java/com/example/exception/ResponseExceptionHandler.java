62627dd5fae9be9d942156266b1412586eeeb09e
Luis Stiven Arteaga Solano - Ceiba Software <luis.arteaga@ceiba.com.co>
2019-09-30 09:21:37 -0500

    cambios temporales

--- src/main/java/com/example/exception/ResponseExceptionHandler.java
+++ src/main/java/com/example/exception/ResponseExceptionHandler.java
@@ -9,11 +9,9 @@ import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@RestController
 @ControllerAdvice
 public class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
 
